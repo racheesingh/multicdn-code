232a7bb72443230e9d2859499e6991718af72355
Rachee Singh <rachee@mail.cs.umass.edu>
2018-09-05 05:32:48 -0400

    plotting prefixes (client/server) code

--- plotting/client_dest_pref_growth.R
+++ plotting/client_dest_pref_growth.R
@@ -20,46 +20,46 @@ common_theme <- function(p) {
 }
 
 options(scipen=10000)
-df<-read.csv("/nfs/kenny/data1/rachee/multicdn/processed_data/per_src_ctn_pfx_counts.csv",
+df<-read.csv("/nfs/kenny/data1/rachee/multicdn/processed_data/per_src_ctn_pfx_counts_msft_v4.csv",
 	header=TRUE)
 df$date <- as.POSIXct(df$ts, origin="1970-01-01", tz="UTC")
 df<-df[df$ctn %in% c('Europe', 'Asia', 'Africa', 'Oceania', 'North America', 'South America'),]
-p <- ggplot(data = df, aes(x=date, y=count, color=ctn)) + geom_point(size=1) + geom_line(size=0.7)
+p <- ggplot(data = df, aes(x=date, y=count, color=ctn)) + geom_line(size=1)
 p <- p + xlab("Time") + ylab("Active client prefixes per day") + coord_cartesian(ylim=c(0, 8000))
-p <- p + scale_x_datetime(breaks = date_breaks("1 month"))
+p <- p + scale_x_datetime(breaks = date_breaks("2 month"))
 common_theme(p)
 ggsave("generated_plots/msft_v4_active_client.pdf", width=12, height=10, limitsize=F)
 
-df<-read.csv("/nfs/kenny/data1/rachee/multicdn/processed_data/per_dst_ctn_pfx_counts.csv",
+df<-read.csv("/nfs/kenny/data1/rachee/multicdn/processed_data/per_dst_ctn_pfx_counts_msft_v4.csv",
 	header=TRUE)
 df$date <- as.POSIXct(df$ts, origin="1970-01-01", tz="UTC")
 df<-df[df$ctn %in% c('Europe', 'Asia', 'Africa', 'Oceania', 'North America', 'South America'),]
-p <- ggplot(data = df, aes(x=date, y=count, color=ctn)) + geom_point(size=1) + geom_line(size=0.7)
+p <- ggplot(data = df, aes(x=date, y=count, color=ctn)) + geom_line(size=1)
 p <- p + xlab("Time") + ylab("Active server prefixes per day") + coord_cartesian(ylim=c(0, 1500))
-p <- p + scale_x_datetime(breaks = date_breaks("1 month"))
+p <- p + scale_x_datetime(breaks = date_breaks("2 month"))
 common_theme(p)
 ggsave("generated_plots/msft_v4_active_dest.pdf", width=12, height=10, limitsize=F)
 
-df<-read.csv("/nfs/kenny/data1/rachee/multicdn/processed_data/per_src_ctn_pfx_counts.csv",
+df<-read.csv("/nfs/kenny/data1/rachee/multicdn/processed_data/per_src_ctn_pfx_counts_msft_v4.csv",
 	header=TRUE)
 df$date <- as.POSIXct(df$ts, origin="1970-01-01", tz="UTC")
 df<-df[df$ctn %in% c('Europe', 'Asia', 'Africa', 'Oceania', 'North America', 'South America'),]
-p <- ggplot(data = df, aes(x=date, y=count, color=ctn))  + geom_line(size=1.2)
+p <- ggplot(data = df, aes(x=date, y=count, color=ctn))  + geom_line(size=1)
 # p <- p + xlab("Time") + ylab("Active client prefixes per day") 
-p <- p + scale_x_datetime(breaks = date_breaks("4 month"), expand=c(0,0)) 
+p <- p + scale_x_datetime(breaks = date_breaks("2 month"), expand=c(0,0)) 
 p <- p + scale_y_log10(limits=c(1, 8000), breaks=c(1, 10, 100, 1000, 6000), expand=c(0,0))
 common_theme(p) + guides(colour = guide_legend(nrow =2)) +
 theme(legend.justification = c(1, 0), legend.position = c(0.95, 0.2)) + 
 xlab("Time") + ylab("Active client prefixes\n per day") 	
 ggsave("generated_plots/msft_v4_active_client_log.pdf", width=8, height=6, limitsize=F)
 
-df<-read.csv("/nfs/kenny/data1/rachee/multicdn/processed_data/per_dst_ctn_pfx_counts.csv",
+df<-read.csv("/nfs/kenny/data1/rachee/multicdn/processed_data/per_dst_ctn_pfx_counts_msft_v4.csv",
 	header=TRUE)
 df<-df[df$ctn %in% c('Europe', 'Asia', 'Africa', 'Oceania', 'North America', 'South America'),]
 df$date <- as.POSIXct(df$ts, origin="1970-01-01", tz="UTC")
-p <- ggplot(data = df, aes(x=date, y=count, color=ctn)) + geom_line(size=1.2)
+p <- ggplot(data = df, aes(x=date, y=count, color=ctn)) + geom_line(size=1)
 # p <- p + xlab("Time") + ylab("Active server prefixes per day") 
-p <- p + scale_x_datetime(breaks = date_breaks("4 month"), expand=c(0,0))
+p <- p + scale_x_datetime(breaks = date_breaks("2 month"), expand=c(0,0))
 p <- p + scale_y_log10(limits=c(1, 2000), breaks=c(1, 10, 100, 1000, 2000), expand=c(0,0))
 common_theme(p) + guides(colour = guide_legend(nrow =2)) +
 theme(legend.justification = c(1, 0), legend.position = c(0.95, 0.2)) +
